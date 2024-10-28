a8b369c1b5656645ee46671bf61601a5fcefbd0a
EricChavarria <eric.chavarria1409@gmail.com>
2024-10-28 16:34:28 -0600

    mok-up home page

--- src/app/page.js
+++ src/app/page.js
@@ -1,101 +1,158 @@
+import { Span } from "next/dist/trace";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.js
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+    <div>
+      <span>
+        Free Courses 🌟 Sale Ends Soon, Get It Now
+      </span>
+      <nav>
+        <ul>
+          <li>Logo</li>
+          <li>Home</li>
+          <li>Courses</li>
+          <li>About Us</li>
+          <li>Princing</li>
+          <li>Contact</li>
+        </ul>
+        <ul>
+          <li>Sing Up</li>
+          <li>Login</li>
+        </ul>
+      </nav>
+      <div>
+        <img src="thunder.svg" />
+        <h1>Unlock Your Creative Potential</h1>
+        <span>
+          with Online Design and Development Courses.
+        </span>
+        <span>
+          Learn from Industry Experts and Enhance Your Skills.
+        </span>
+        <div>
+          <button>Explore Coruses</button>
+          <button>View Princing</button>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+      </div>
+      <div>
+        <img src='zapier.svg' />
+        <img src='spotify.svg' />
+        <img src='zoom.svg' />
+        <img src='amazon.svg' />
+        <img src='adobe.svg' />
+        <img src='notion.svg' />
+        <img src='netflix.svg' />
+      </div>
+      <img src='video' />
+      <section>
+        <div>
+          <div>
+            <h3>Benefits</h3>
+            <p>
+              Lorem ipsum dolor sit amet consectetur. Tempus tincidunt etiam eget elit id imperdiet et. Cras eu sit dignissim lorem nibh et. Ac cum eget habitasse in velit fringilla feugiat senectus in.
+            </p>
+          </div>
+          <button>View All</button>
+        </div>
+        <div>
+          <h4>01</h4>
+          <span>Flexible Learning Schedule</span>
+          <p>Fit your coursework around your existing commitments and obligations.</p>
+        </div>
+      </section>
+      <section>
+        <div>
+          <div>
+            <h3>Our Courses</h3>
+            <p>
+              Lorem ipsum dolor sit amet consectetur. Tempus tincidunt etiam eget elit id imperdiet et. Cras eu sit dignissim lorem nibh et. Ac cum eget habitasse in velit fringilla feugiat senectus in.
+            </p>
+          </div>
+          <button>View All</button>
+        </div>
+        <div>
+          <img src='working.png' />
+          <div>
+            <div>
+              <span>4 Weeks</span>
+              <span>Beginner</span>
+            </div>
+            <span>By Jhon Smith</span>
+          </div>
+          <h3>Web Design Fundamentals</h3>
+          <p>
+            Learn the fundamentals of web design, including HTML, CSS, and responsive design principles. Develop the skills to create visually appealing and user-friendly websites.
+          </p>
+          <button>Get it Now</button>
+        </div>
+      </section>
+      <section>
+        <div>
+          <div>
+            <h3>Our Pricing</h3>
+            <p>
+              Lorem ipsum dolor sit amet consectetur. Tempus tincidunt etiam eget elit id imperdiet et. Cras eu sit dignissim lorem nibh et. Ac cum eget habitasse in velit fringilla feugiat senectus in.
+            </p>
+          </div>
+          <div>
+            <button>Monthly</button>
+            <button>Yearly</button>
+          </div>
+        </div>
+        <div>
+          <div>
+            <span>Free plan</span>
+            <h3>
+              $0
+              <span>/month</span>
+            </h3>
+            <div>
+              <span>Available Features</span>
+              <ul>
+                <li>
+                  <img src='check.svg' />
+                  <span>Access to selected free courses.</span>
+                </li>
+                <li>
+                  <img src='check.svg' />
+                  <span>Limited course materials and resources.</span>
+                </li>
+                <li>
+                  <img src='check.svg' />
+                  <span>Basic community support.</span>
+                </li>
+                <li>
+                  <img src='check.svg' />
+                  <span>No certification upon completion.</span>
+                </li>
+                <li>
+                  <img src='check.svg' />
+                  <span>Ad-supported platform.</span>
+                </li>
+                <li>
+                  <img src='unCheck.svg' />
+                  <span>Access to exclusive Pro Plan community forums.</span>
+                </li>
+                <li>
+                  <img src='unCheck.svg' />
+                  <span>Early access to new courses and updates.</span>
+                </li>
+              </ul>
+              <button>Get Started</button>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section>
+        <div>
+          <h3>Frequently Asked Questions</h3>
+          <p>
+            Still you have any questions? Contact our Team via support@skillbridge.com
+          </p>
+          <button>See All FAQ’s</button>
+        </div>
+      </section>
     </div>
   );
 }
